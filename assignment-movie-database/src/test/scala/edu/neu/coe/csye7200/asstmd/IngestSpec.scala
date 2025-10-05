faa6424bfd11278af8137609813eb27b40788e0f
Mayukh Sinha <sinha.may@northeastern.edu>
2025-10-05 19:25:47 -0400

    Assignment: Movie Database part 2

--- assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
+++ assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
@@ -21,7 +21,8 @@ class IngestSpec extends AnyFlatSpec with Matchers {
     val ingester = new Ingest[Int]()
     val xys = ingester(source).toSeq
     // check that xys has exactly one element, consisting of Success(42) -- 10 points
-    // TO BE IMPLEMENTED 
+    xys should have length 1
+    xys.head shouldBe Success(42)
     // END
   }
 
